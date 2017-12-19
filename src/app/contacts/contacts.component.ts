f2719b9f417440ed385b6decf49d7414c94536de
nizam <nizam9>
2017-12-19 16:29:59 +0530

    project setup

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'contacts',
+    templateUrl: 'contacts.component.html',
+    styleUrls: ['contacts.component.scss']
+})
+export class ContactsComponent {
+
+}
